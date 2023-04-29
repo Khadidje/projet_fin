c1031da319e8e8d7bc1943ea357eca9ec8d572c7
Khadidje <101344256+Khadidje@users.noreply.github.com>
2023-04-29 13:43:21 +0200

    Update ann.py

--- ann.py
+++ ann.py
@@ -4,8 +4,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('C:\Users\kaditch\Desktop\CNN\input')
 
 @st.cache_data
 def load_data(nrows):
